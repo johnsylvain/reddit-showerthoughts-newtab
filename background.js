0d887df17de251913a3b90cdaf1c984be7a3dacb
John Sylvain <john.n.sylvain@gmail.com>
2017-06-18 15:47:29 -0500

    fetches random post from reddit api

--- background.js
+++ background.js
@@ -1,24 +1,51 @@
 function App() {
   this.thought = null
   this.view = document.getElementById('view');
+  this.mainTmpl = 'main_tmpl'
+  this.cache = {};
 }
+
+App.prototype.init = function () {
+  this.getThought();
+};
 
 App.prototype.getThought = function() {
   var vm = this;
 
-  // mock api request
-  setTimeout(function() {
-    vm.thought = {
-      post: "nice",
-      author: "/u/username"
-    }
-    vm.renderView()
-  }, 1000)
+  fetch('https://www.reddit.com/r/showerthoughts/hot.json')
+    .then(function(data) {
+      return data.json()
+    })
+    .then(function(res) {
+      var thought = res.data.children[Math.floor(Math.random() * res.data.children.length)];
+      vm.thought = {
+        post: thought.data.title,
+        author: '/u/' + thought.data.author
+      }
+      vm.view.innerHTML = vm.renderView(vm.mainTmpl, vm.thought)
+    })
+
 }
 
-App.prototype.renderView = function () {
-  this.view.innerHTML = JSON.stringify(this.thought, null, ' ');
+App.prototype.renderView = function (str, data) {
+  var fn = !/\W/.test(str) ?
+    this.cache[str] = this.cache[str] ||
+      this.renderView(document.getElementById(str).innerHTML) :
+    new Function("obj",
+      "var p=[],print=function(){p.push.apply(p,arguments);};" +
+      "with(obj){p.push('" +
+      str
+        .replace(/[\r\t\n]/g, " ")
+        .split("<%").join("\t")
+        .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+        .replace(/\t=(.*?)%>/g, "',$1,'")
+        .split("\t").join("');")
+        .split("%>").join("p.push('")
+        .split("\r").join("\\'")
+    + "');}return p.join('');");
+
+  return data ? fn( data ) : fn;
 };
 
 var app = new App();
-app.getThought();
+app.init();
